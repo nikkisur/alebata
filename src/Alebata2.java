285acbf560d974156eb213c17e90a038849b59d9
Kaye Donato <katrinadonato@yahoo.com>
2014-02-22 13:47:48 +0800

    Grammar1

--- src/Alebata2.java
+++ src/Alebata2.java
@@ -6,52 +6,142 @@ import java.util.Scanner;
 
 
 public class Alebata2 {
-		
-		// variables for checking validity of inputted variable names
-		static String character = "abcdefghijklmnopqrstuvwxyz";
-		static String character2 = "0123456789-_abcdefghjiklmnopqrstuvwxyz";
-		static String number = "0123456789-";
-		
-		// a list for storing inputted variable names so they can be checked for validity
-		static HashMap<String, String> variables = new HashMap<String, String>();
-				
-		// a variable for storing one inputted line
-		static String line;
-		
-		// a variable for scanning the contents of one inputted line
-		static Scanner scan, scanLine;
-		static File lookUp;
-		static HashMap<String, String> reservedWords = new HashMap<String, String>();
-		
-		public static void main(String args[]){
-			// a variable for scanning every line of user input
-//			File file = new File(args[0]);
-			File file = new File("ale.bata");
-			try{
-				scan = new Scanner(file);
-				
-			}
-			catch(Exception e){
-				System.out.println("File not found.");
-			}
-			addReservedWords();
-			action();
+
+	// variables for checking validity of inputted variable names
+	static String character = "abcdefghijklmnopqrstuvwxyz";
+	static String character2 = "0123456789-_abcdefghjiklmnopqrstuvwxyz";
+	static String number = "0123456789-";
+
+	// a list for storing inputted variable names so they can be checked for validity
+	static HashMap<String, String> variables = new HashMap<String, String>();
+
+	// a variable for storing one inputted line
+	static String line;
+
+	// a variable for scanning the contents of one inputted line
+	private static Scanner scan, scanLine;
+	private static File lookUp;
+	private static HashMap<String, String> reservedWords = new HashMap<String, String>();
+	private static ArrayList<String> tokens = new ArrayList<String>();
+	private static ArrayList<String> lexemes = new ArrayList<String>();
+
+	private static int index = 0;
+	private static String token;
+
+	public static void main(String args[]){
+		// a variable for scanning every line of user input
+		//			File file = new File(args[0]);
+		File file = new File("ale.bata");
+		try{
+			scan = new Scanner(file);
+
 		}
-		
-		public static void action(){
-			while(scan.hasNext()){
-				String[] words = scan.nextLine().split("(?<=\\.)|(?=\\.)| ");
-				for(String word : words){
-					if(reservedWords.get(word) != null){
-						System.out.println(word + "			" + reservedWords.get(word));
-					}
-					else{
-						System.out.println(word + "			" + "IDENT");
-					}
+		catch(Exception e){
+			System.out.println("File not found.");
+		}
+		addReservedWords();
+		action();
+		getNextToken();
+		A();
+	}
+
+	public static void action(){
+		while(scan.hasNext()){
+			String[] words = scan.nextLine().split("(?<=\\!)|(?=\\!)| ");
+			for(String word : words){
+				if(reservedWords.get(word) != null){
+					tokens.add(reservedWords.get(word));
+					lexemes.add(word);
+										System.out.println(word + "			" + reservedWords.get(word));
+				}
+				else{
+					tokens.add("IDENT");
+					lexemes.add(word);
+										System.out.println(word + "			" + "IDENT");
 				}
 			}
-			
 		}
+
+	}
+
+	public static void getNextToken(){
+		if(index == tokens.size())
+			token = null;
+		else
+			token = tokens.get(index++);
+	}
+
+	//A -> GAWAB 
+	public static void A(){
+		while(token != null){
+			if(token.equals("TYPE1")){
+				getNextToken();
+				B();
+			}
+			else if(token.equals("IDENT")){
+				getNextToken();
+				C();
+			}
+			getNextToken();
+		}
+	}
+	
+	//B -> NG
+	public static void B(){
+		if(token != null){
+			if(token.equals("TYPE2")){
+				getNextToken();
+				while(token!= null && token.equals("IDENT")){
+					checkVar(lexemes.get(index-1));
+					getNextToken();
+				}
+				if(token == null || !token.equals("TERMINATOR")){
+					System.out.println("Missing ! at the end of line");
+					close();
+				}
+			}
+			else{
+				System.out.println("Syntax should be: GAWA NG <var_name>!");
+				close();
+			}
+		}
+		else{
+			System.out.println("Syntax should be: GAWA NG <var_name>!");
+			close();
+		}
+	}
+	
+	public static void C(){
+		if(variables.get(lexemes.get(index-2)) != null){
+			String var = lexemes.get(index-2);
+			if(token.equals("EQUALS")){
+				getNextToken();
+				if(token.equals("DQUOTE")){
+					System.out.println("her");
+					getNextToken();
+					String value = "";
+					while(!token.equals("DQUOTE")){
+						value += lexemes.get(index-1);
+						getNextToken();
+						if(token == null){
+							System.out.println("Missing ! at the end of line");
+							close();
+						}
+					}
+					System.out.println(var + " = " + value);
+				}
+				else{
+					//math op
+				}
+			}
+			else{
+				System.out.println("Syntax should be: <var_name> AY <value>");
+			}
+		}
+		else{
+			System.out.println(lexemes.get(index-2) + " must be initialized.");
+		}
+	}
 
 	// method for checking validity of variable name
 	// returns true
@@ -80,23 +170,10 @@ public class Alebata2 {
 					for(int b = 1; b < temp.length(); b++){
 						// the case where a character in the variable name is not in the set of valid characters
 						if(character2.indexOf(String.valueOf(temp.charAt(b))) == -1){
-							// the inner case where that character is the last character of the input line
-							if(a == vars.length-1 && b == temp.length()-1){
-								// the inner case where that character is a period
-								if(temp.charAt(temp.length()-1) == '.'){
-									if(variables.get(temp.substring(0, b)) != null)
-										System.out.println("	Mayroon nang variable na ang pangalan ay " + temp);
-									else{
-										variables.put(temp.substring(0, b), "");
-										System.out.println("	Gumawa ka ng variable na ang pangalan ay " + temp);
-									}
-									return true;
-								}
-							}
 							// this is only reached if an invalid character is reached
 							// the only invalid character allowed is a period and that's if it's the last character of the line -- signifying the end of a line in the "Ale, Bata" language
 							System.out.println("	Maari lamang ang a-z, 0-9,- at _.");
-							break;
+							close();
 						}
 						// the case where all characters in the variable name are valid and we're iterating through the last character of said name
 						else if(b == temp.length()-1){
@@ -113,7 +190,7 @@ public class Alebata2 {
 		}
 		return true;
 	}
-	
+
 	//method to add the reserved words to a hashmap
 	public static void addReservedWords(){
 		try{
@@ -125,6 +202,11 @@ public class Alebata2 {
 		}catch(Exception e){
 			System.out.println("Can't find 'ReservedWords.txt'");
 		}
+
+	}
+	
+	public static void close(){
+		System.exit(0);
 		
 	}
 }
